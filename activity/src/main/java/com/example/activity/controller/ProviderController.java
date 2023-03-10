d950a7e9c2b792fec488aa0df5177b8c96f1ffa9
Zeemann Lin <zeemann.lin@ingka.ikea.com>
2023-03-10 17:14:27 +0800

    update: add mybatis-plus pom

--- activity/src/main/java/com/example/activity/controller/ProviderController.java
+++ activity/src/main/java/com/example/activity/controller/ProviderController.java
@@ -3,7 +3,6 @@ package com.example.activity.controller;
 import com.example.activity.api.ProviderApi;
 import com.example.common.response.ResponseResult;
 import com.example.common.response.code.ErrorCode;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
@@ -11,7 +10,6 @@ import java.util.HashMap;
 @RestController
 public class ProviderController implements ProviderApi {
 
-    @GetMapping("/provider")
     public ResponseResult provider(){
         HashMap<String, Boolean> hashmap = new HashMap<>();
         hashmap.put("1", ResponseResult.success().isSuccess());
