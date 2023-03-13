7ac7b27edd69bb6ba3ffa34b1ad2d69018e73905
Zeemann Lin <zeemann.lin@ingka.ikea.com>
2023-03-13 13:30:29 +0800

    feat: paging operation

--- activity/src/main/java/com/example/activity/api/ProviderApi.java
+++ activity/src/main/java/com/example/activity/api/ProviderApi.java
@@ -1,7 +1,9 @@
 package com.example.activity.api;
 
 import com.example.common.response.ResponseResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 public interface ProviderApi {
     @GetMapping("/provider")
@@ -9,4 +11,10 @@ public interface ProviderApi {
 
     @GetMapping("/mysqlTest")
     ResponseResult mysqlTest();
+
+    @GetMapping("/pageSearch")
+    ResponseResult pageSelect();
+
+    @DeleteMapping("/delete/{id}")
+    ResponseResult mysqlDelete(@PathVariable Long id);
 }
--- activity/src/main/java/com/example/activity/controller/ProviderController.java
+++ activity/src/main/java/com/example/activity/controller/ProviderController.java
@@ -1,10 +1,13 @@
 package com.example.activity.controller;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.example.activity.api.ProviderApi;
+import com.example.activity.domain.UserInfo;
 import com.example.activity.mapper.UserInfoMapper;
 import com.example.common.response.ResponseResult;
 import com.example.common.response.code.ErrorCode;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
@@ -15,6 +18,7 @@ public class ProviderController implements ProviderApi {
 
     private final UserInfoMapper userInfoMapper;
 
+    @Override
     public ResponseResult provider(){
         HashMap<String, Boolean> hashmap = new HashMap<>();
         hashmap.put("1", ResponseResult.success().isSuccess());
@@ -28,5 +32,22 @@ public class ProviderController implements ProviderApi {
     @Override
     public ResponseResult mysqlTest() {
         return ResponseResult.success(userInfoMapper.selectList(null));
+    }
+
+    @Override
+    public ResponseResult pageSelect() {
+        Page<UserInfo> page = new Page<>(1, 10);
+        userInfoMapper.selectPage(page,null);
+        return ResponseResult.success(page.getRecords());
+
+    }
+
+    @Override
+    public ResponseResult mysqlDelete(@PathVariable Long id) {
+        int status = userInfoMapper.deleteById(id);
+        if(status == 1){
+            return ResponseResult.success();
+        }
+        return ResponseResult.failed();
     }
 }
