04f2f6d9c35d0f7bf8d3317a40945dd8d54b8292
Zeemann Lin <zeemann.lin@ingka.ikea.com>
2023-03-14 14:14:49 +0800

    feat: add springcloud-gateway
    gateway作为统一入口，具有路由转发的功能

---
+++ gateway/src/main/java/com/example/gateway/GateApplication.java
@@ -0,0 +1,13 @@
+package com.example.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication()
+public class GateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GateApplication.class, args);
+    }
+
+}
